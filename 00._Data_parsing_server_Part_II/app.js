b91a644b070dd9993b920d4896e0d03edb5782ad
Wei Yang <weix0015@stud.kea.dk>
2025-02-06 14:04:02 +0100

    tilføjet app.js til at server files / data til text based data formats

---
+++ 00._Data_parsing_server_Part_II/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+
+const app = express();
+const BASE_FOLDER = path.join("..", "02_Text-based_Data_Formats");
+const FILE_NAME = "me";
+const ENCODE = "utf-8";
+
+app.get("/json", (req, res) => {
+    const data = fs.readFileSync(path.join(BASE_FOLDER, `${FILE_NAME}.json`), ENCODE);
+    res.send(JSON.stringify(data, null, 4));
+});
+
+app.get("/csv", (req, res) => {
+    const data = fs.readFileSync(path.join(BASE_FOLDER, `${FILE_NAME}.csv`), ENCODE);
+    res.send(data);
+});
+
+app.get("/xml", (req, res) => {
+    const data = fs.readFileSync(path.join(BASE_FOLDER, `${FILE_NAME}.xml`), ENCODE);
+    res.send(data);
+});
+
+app.get("/yaml", (req, res) => {
+    const data = fs.readFileSync(path.join(BASE_FOLDER, `${FILE_NAME}.yaml`), ENCODE);
+    res.send(data);
+});
+
+app.get("/txt", (req, res) => {
+    const data = fs.readFileSync(path.join(BASE_FOLDER, `${FILE_NAME}.txt`), ENCODE);
+    res.send(data);
+});
+
+const PORT = 8080;
+app.listen(PORT, () => console.log("Server running on port", PORT));
