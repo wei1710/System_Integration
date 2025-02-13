10e1992ee36214b4e86a4334872473b6bf22d953
Wei Yang <weix0015@stud.kea.dk>
2025-02-13 18:28:55 +0100

    lavet data parsing server part 3

---
+++ 03a_[Individual] Data parsing server - Part III/01_node/app.js
@@ -0,0 +1,70 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+
+const app = express();
+const BASE_FOLDER = path.join("..", "..", "02_Text-based_Data_Formats");
+const FILE_NAME = "me";
+const ENCODE = "utf-8";
+const BASE_URL = "http://127.0.0.1:8000";
+
+app.get("/json", (req, res) => {
+    const data = fs.readFileSync(path.join(BASE_FOLDER, `${FILE_NAME}.json`), ENCODE);
+    res.json(JSON.parse(data));
+});
+
+app.get("/csv", (req, res) => {
+    const data = fs.readFileSync(path.join(BASE_FOLDER, `${FILE_NAME}.csv`), ENCODE);
+    res.send(`<pre>${data}</pre>`);
+});
+
+app.get("/xml", (req, res) => {
+    const data = fs.readFileSync(path.join(BASE_FOLDER, `${FILE_NAME}.xml`), ENCODE);
+    res.send(data);
+});
+
+app.get("/yaml", (req, res) => {
+    const data = fs.readFileSync(path.join(BASE_FOLDER, `${FILE_NAME}.yaml`), ENCODE);
+    res.send(data);
+});
+
+app.get("/txt", (req, res) => {
+    const data = fs.readFileSync(path.join(BASE_FOLDER, `${FILE_NAME}.txt`), ENCODE);
+    res.send(data);
+});
+
+app.get("/get_json", async (req, res) => {
+    const response = await fetch(`${BASE_URL}/json`);
+    const result = await response.json();
+    res.send(result)
+});
+
+app.get("/get_csv", async (req, res) => {
+    const response = await fetch(`${BASE_URL}/csv`);
+    const result = await response.text();
+    res.send(`<pre>${result}</pre>`);
+});
+
+app.get("/get_xml", async (req, res) => {
+    const response = await fetch(`${BASE_URL}/xml`);
+    const result = await response.text();
+    res.setHeader('Content-Type', 'text/xml');
+    res.send(result);
+});
+
+app.get("/get_yaml", async (req, res) => {
+    const response = await fetch(`${BASE_URL}/yaml`);
+    const result = await response.text();
+    res.setHeader('Content-Type', 'text/yaml');
+    res.send(result);
+});
+
+app.get("/get_txt", async (req, res) => {
+    const response = await fetch(`${BASE_URL}/txt`);
+    const result = await response.text();
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(result);
+});
+
+const PORT = 8080;
+app.listen(PORT, () => console.log("Server running on port", PORT));
---
+++ 03a_[Individual] Data parsing server - Part III/02_python/main.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI, Response
+import os, json, requests
+
+app = FastAPI()
+BASE_FOLDER = os.path.join("..", "..", "02_Text-based_Data_Formats")
+FILE_NAME = "me"
+ENCODE = "utf-8"
+BASE_URL = "http://127.0.0.1:8080";
+
+@app.get("/json")
+def json_endpoint():
+  file_path = os.path.join(BASE_FOLDER, f"{FILE_NAME}.json")
+  with open(file_path, encoding = ENCODE) as file:
+    data = json.load(file)
+  return data
+
+@app.get("/csv")
+def csv_endpoint():
+  file_path = os.path.join(BASE_FOLDER, f"{FILE_NAME}.csv")
+  with open(file_path, encoding = ENCODE) as file:
+    data = file.read()
+  return Response(content = data)
+
+@app.get("/xml")
+def xml_endpoint():
+  file_path = os.path.join(BASE_FOLDER, f"{FILE_NAME}.xml")
+  with open(file_path, encoding = ENCODE) as file:
+    data = file.read()
+  return Response(content = data)
+
+@app.get("/yaml")
+def yaml_endpoint():
+  file_path = os.path.join(BASE_FOLDER, f"{FILE_NAME}.yaml")
+  with open(file_path, encoding = ENCODE) as file:
+    data = file.read()
+  return Response(content = data)
+
+@app.get("/txt")
+def txt_endpoint():
+  file_path = os.path.join(BASE_FOLDER, f"{FILE_NAME}.txt")
+  with open(file_path, encoding = ENCODE) as file:
+    data = file.read()
+  return Response(content = data)
+
+@app.get("/get_json")
+def get_json():
+  response = requests.get(f"{BASE_URL}/json")
+  return response.json()
+
+@app.get("/get_csv")
+def get_csv():
+  response = requests.get(f"{BASE_URL}/csv")
+  return Response(content = response.text)
+  
+@app.get("/get_xml")
+def get_xml():
+  response = requests.get(f"{BASE_URL}/xml")
+  return Response(content = response.text)
+
+@app.get("/get_yaml")
+def get_yaml():
+  response = requests.get(f"{BASE_URL}/yaml")
+  return Response(content = response.text)
+
+@app.get("/get_txt")
+def get_txt():
+  response = requests.get(f"{BASE_URL}/txt")
+  return Response(content = response.text)
